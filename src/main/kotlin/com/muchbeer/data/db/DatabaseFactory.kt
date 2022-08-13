d8c13fd9f029079ba9bd10b1123c4563d486e325
muchbeer <gman2them@gmail.com>
2022-08-13 23:33:29 +0300

    working on postgres

--- src/main/kotlin/com/muchbeer/data/db/DatabaseFactory.kt
+++ src/main/kotlin/com/muchbeer/data/db/DatabaseFactory.kt
@@ -48,7 +48,7 @@ object DatabaseFactory {
     fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER") // 1
-        config.jdbcUrl = System.getenv("DATABASE_URL") // 2
+        config.jdbcUrl = System.getenv("DATABASE_URL_TAP") // 2
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
